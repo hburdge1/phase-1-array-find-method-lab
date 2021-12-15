7071f6f0aaf84e9e6b6286eebe09a7bfdb717d85
Hannah Burdge <hburdge1@gmail.com>
2021-12-15 14:30:22 -0800

    commit tree 37830db3a4a83b3d2afa66aa95169267c8ea27f1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    const game = record.find(gameWon);
+        if (game){
+            return game.year
+        }
+}
+function gameWon(game) {
+    return game.result === 'W';
+}
